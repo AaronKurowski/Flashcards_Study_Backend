71b53c715339b0fc02c7c7818a0a1ae68f27b65a
Aaron Kurowski <aaron12342@att.net>
2021-06-03 14:14:09 -0500

    Installed corsheaders

--- flashcards_database/flashcards_database/settings.py
+++ flashcards_database/flashcards_database/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'flashcards.apps.FlashcardsConfig'
+    'flashcards.apps.FlashcardsConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'flashcards_database.urls'
@@ -127,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
