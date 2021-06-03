dced9fb973702d2c8314f57565cf3cfe07aba876
Aaron Kurowski <aaron12342@att.net>
2021-06-03 13:15:44 -0500

    All of the user stories for this api completed except for putting/posting flashcards. Switching gears for now

--- flashcards_database/flashcards/urls.py
+++ flashcards_database/flashcards/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('collection/', views.CollectionList.as_view()),
-    path('collection/<int:pk>/', views.CollectionDetail.as_view()),
-    path('collection/<int:pk>/flashcard/', views.FlashcardList.as_view())
+    path('collection/<int:collection_id>/', views.CollectionDetail.as_view()),
+    path('collection/<int:collection_id>/flashcard/', views.FlashcardList.as_view()),
+    path('collection/<int:collection_id>/flashcard/<int:card_id>/', views.FlashcardDetail.as_view()),
 ]
--- flashcards_database/flashcards/views.py
+++ flashcards_database/flashcards/views.py
@@ -27,40 +27,50 @@ class CollectionList(APIView):
 # gets collection by id
 class CollectionDetail(APIView):
 
-    def get_collection(self, pk):
+    def get_collection(self, collection_id):
         try:
-            return Collection.objects.get(pk=pk)
+            return Collection.objects.get(pk=collection_id)
         except Collection.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
-        collection = self.get_collection(pk)
+    def get(self, request, collection_id):
+        collection = self.get_collection(collection_id)
         serializer = CollectionSerializer(collection)
         return Response(serializer.data)
 
 
 class FlashcardList(APIView):
 
-    def get(self, request, pk):
-        flashcard = Flashcard.objects.filter(collection=pk)
+    def get(self, request, collection_id):
+        flashcard = Flashcard.objects.filter(collection=collection_id)
         serializer = FlashcardSerializer(flashcard, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+    def post(self, request, collection_id):
         serializer = FlashcardSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#
-# class FlashcardDetail(APIView):
-#
-#     def get_object(self, pk):
-#         try:
-#             return Flashcard.objects.get(pk=pk)
-#         except Flashcard.DoesNotExist:
-#             raise Http404
-#
-#     def get_object_by_collection(self, fk):
-#         return
+
+class FlashcardDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Flashcard.objects.get(pk=pk)
+        except Flashcard.DoesNotExist:
+            raise Http404
+
+    def put(self, request, collection_id, card_id):
+        flashcard = self.get_object(card_id)
+        serializer = FlashcardSerializer(flashcard, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, collection_id, card_id):
+        flashcard = self.get_object(card_id)
+        flashcard.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
